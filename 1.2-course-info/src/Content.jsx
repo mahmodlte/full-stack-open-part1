9a4279236f9ea40d3d955592072cb37cbef0bc9c
Mahmoud <mahmodlte@gmail.com>
2025-02-24 14:31:09 +0300

    Refactored the Content component and created a reusable component to use insted of hardcoding the content

--- 1.2-course-info/src/Content.jsx
+++ 1.2-course-info/src/Content.jsx
@@ -1,15 +1,12 @@
+import ContentPart from "./ContentPart";
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <ContentPart part={props.part1} exercise={props.exercises1} />
+      <ContentPart part={props.part2} exercise={props.exercises2} />
+
+      <ContentPart part={props.part3} exercise={props.exercises3} />
     </div>
   );
 };
---
+++ 1.2-course-info/src/ContentPart.jsx
@@ -0,0 +1,11 @@
+const ContentPart = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </>
+  );
+};
+
+export default ContentPart;
