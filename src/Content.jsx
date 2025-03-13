c857eddadd29811b597d01bfe84d4ea9da9b7304
Mahmoud <mahmodlte@gmail.com>
2025-03-13 14:03:57 +0300

    Used the parts data array in the Content and passed each single part to ContentPart component

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,10 +3,9 @@ import ContentPart from "./ContentPart";
 const Content = (props) => {
   return (
     <div>
-      <ContentPart part={props.part1.name} exercise={props.part1.exercises} />
-      <ContentPart part={props.part2.name} exercise={props.part2.exercises} />
-
-      <ContentPart part={props.part3.name} exercise={props.part3.exercises} />
+      {props.parts.map((part) => {
+        return <ContentPart key={part.exercises} part={part} />;
+      })}
     </div>
   );
 };
