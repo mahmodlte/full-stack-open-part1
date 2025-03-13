661f7c65b052de7c39b0999c13c3c818c59789ac
Mahmoud <mahmodlte@gmail.com>
2025-03-13 13:32:25 +0300

    Changed the names of props calling to the new names defined in the objects

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,10 +3,10 @@ import ContentPart from "./ContentPart";
 const Content = (props) => {
   return (
     <div>
-      <ContentPart part={props.part1} exercise={props.exercises1} />
-      <ContentPart part={props.part2} exercise={props.exercises2} />
+      <ContentPart part={props.part1.name} exercise={props.part1.exercises} />
+      <ContentPart part={props.part2.name} exercise={props.part2.exercises} />
 
-      <ContentPart part={props.part3} exercise={props.exercises3} />
+      <ContentPart part={props.part3.name} exercise={props.part3.exercises} />
     </div>
   );
 };
