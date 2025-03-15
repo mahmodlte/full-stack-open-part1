070ea5798a89ed99d37fee602fd0e929d9377168
Mahmoud <mahmodlte@gmail.com>
2025-03-15 10:15:45 +0300

    Implemented the Total number of exercises

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,8 +1,12 @@
 const Total = (props) => {
+  const totalExercises =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
   return (
     <div>
       <p>
-        <p>Number of exercises {props.totalExercises}</p>
+        <p>Number of exercises {totalExercises}</p>
       </p>
     </div>
   );
