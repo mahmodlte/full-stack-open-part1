81b17bb68073f4e4f53ab15ba9576bb7c9e88b90
Mahmoud <mahmodlte@gmail.com>
2025-03-19 10:41:32 +0300

    changed the app to only display statistics when there is statistics data

--- unicafe/src/Statistics.jsx
+++ unicafe/src/Statistics.jsx
@@ -5,18 +5,28 @@ const Statistics = ({ good, bad, natural }) => {
     return postiveFeedback * 100;
   };
   return (
-    <div>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Natural: {natural}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good + natural + bad}</p>
-      <p>Average: {(good + bad + natural) / 3}</p>
-      <p>
-        Postive:
-        {getPostiveFeedback() ? getPostiveFeedback() : "There is no Feedback"}%
-      </p>
-    </div>
+    <>
+      {good || bad || natural ? (
+        <div>
+          {" "}
+          <h1>Statistics</h1>
+          <p>Good: {good}</p>
+          <p>Natural: {natural}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {good + natural + bad}</p>
+          <p>Average: {(good + bad + natural) / 3}</p>
+          <p>
+            Postive:
+            {getPostiveFeedback()
+              ? getPostiveFeedback()
+              : "There is no Feedback"}
+            %
+          </p>{" "}
+        </div>
+      ) : (
+        <p>There is no feedback</p>
+      )}
+    </>
   );
 };
 
