b0595b8abe8d9482b2a52766591320a29ce7c295
Mahmoud <mahmodlte@gmail.com>
2025-03-19 10:23:09 +0300

    exercise 1.6: built the unicafe customer feedback

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import Button from "./Button";
 function App() {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [natural, setNatural] = useState(0);
+  const handleGood = () => setGood(good + 1);
+  const handleBad = () => setBad(bad + 1);
+  const handleNatural = () => setNatural(natural + 1);
   return (
     <>
-      <div></div>
+      <div>
+        <h1>Give Feedback</h1>
+        <Button text={"Good"} onClick={handleGood} />
+
+        <Button text="Natural" onClick={handleNatural} />
+        <Button text={"Bad"} onClick={handleBad} />
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Natural: {natural}</p>
+        <p>Bad: {bad}</p>
+      </div>
     </>
   );
 }
