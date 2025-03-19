7f967f02595d7313ea4e3cf8b4f7bf705e840e17
Mahmoud <mahmodlte@gmail.com>
2025-03-19 11:26:28 +0300

    Implemented table for displaying statistics data

--- unicafe/src/StatisticLine.jsx
+++ unicafe/src/StatisticLine.jsx
@@ -1,9 +1,40 @@
-const StatisticLine = ({ text, value }) => {
+const StatisticLine = ({ statisticData }) => {
+  console.log(statisticData);
+
   return (
     <div>
-      <p>
-        {text}: {value}
-      </p>
+      <table>
+        <tbody>
+          <tr>
+            <td>{statisticData.good.text}</td>
+            <td>{statisticData.good.value}</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>{statisticData.bad.text}</td>
+            <td>{statisticData.bad.value}</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>{statisticData.natural.text}</td>
+            <td>{statisticData.natural.value}</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>{statisticData.total.text}</td>
+            <td>{statisticData.total.value}</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>{statisticData.average.text}</td>
+            <td>{statisticData.average.value}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
